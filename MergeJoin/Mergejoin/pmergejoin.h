f21ed4bc0973814e4991cbc307bd99b3116674c9
Henry heh <Emhrk@outlook.com>
2024-04-28 23:28:16 -0400

    sync median search

--- MergeJoin/Mergejoin/pmergejoin.h
+++ MergeJoin/Mergejoin/pmergejoin.h
@@ -2,6 +2,7 @@
 #include <list>
 #include <algorithm>
 #include <future>
+#include <functional>
 
 #include <boost/asio.hpp>
 #include <boost/bind.hpp>
@@ -50,6 +51,62 @@ void PMergeSort(std::vector<T>& arr, std::size_t n_worker, const Compare& cmp)
 
 	arr = jobs.front();  //! copy???
 	return;
+}
+
+
+/// <summary>
+/// find the median val in M1 \bigcup M2
+/// return median value we dont care about index or where it from so just val
+/// </summary>
+template <typename T, typename Compare>
+T FindMedian(std::vector<T> const& m1, std::vector<T> const& m2)
+{
+	auto less_than = [](std::vector<T> const& left, std::vector<T> const& right, 
+						std::size_t const& lidx, std::size_t const& ridx) {
+		if (lidx >= left.size() && ridx >= right.size())
+			return false;
+		if (lidx >= left.size())
+			return false;
+		if (ridx >= right.size())
+			return true;
+
+		return left[lidx] < right[ridx];
+	};
+
+	// case median \in m1
+	std::size_t begin = 0;
+	std::size_t end = m1.size() + m2.size();
+	std::size_t e_val = end / 2;
+
+	while (begin < end) {
+		auto mid = (begin + end) / 2;
+		if (less_than(m1, m2, mid, e_val - mid - 1)) {
+			begin = mid + 1;
+		} else if (less_than(m2, m1, e_val - mid, mid)) {
+			end = mid;
+		} else if (less_than(m2, m1, e_val - mid - 1, mid) && less_than(m1, m2, mid, e_val - mid)) {
+			return m1[mid];
+		}
+	}
+
+	// case median \in m2
+	std::size_t begin = 0;
+	std::size_t end = m1.size() + m2.size();
+
+	while (begin < end) {
+		auto mid = (begin + end) / 2;
+		if (less_than(m2, m1, mid, e_val - mid - 1)) {
+			begin = mid + 1;
+		}
+		else if (less_than(m1, m2, e_val - mid, mid)) {
+			end = mid;
+		}
+		else if (less_than(m1, m2, e_val - mid - 1, mid) && less_than(m2, m1, mid, e_val - mid)) {
+			return m2[mid];
+		}
+	}
+
+	throw std::runtime_error("FindMedian didnt find median in both m1 and m2!");
 }
 
 
